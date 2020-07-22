4f8b02dca2a1c08362f422571abbd2ca166752b7
Ivy Leong <ivy728@gmail.com>
2020-07-21 22:37:49 -0700

    Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -24,6 +24,10 @@ function buildTable(data) {
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
+    let city=d3.select("#city").property("value");
+    let state=d3.select("#state").property("value");
+    let country=d3.select("#country").property("value");
+    let shape=d3.select("#shape").property("value");
     let filteredData = tableData;
     
      // Check to see if a date was entered and filter the
@@ -32,11 +36,31 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (city) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.city === city);
+    }
+    if (state) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.state === state);
+    }
+    if (country) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.country === country);
+    }
+    if (shape) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.shape === shape);
     };
-    
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
+   
     buildTable(filteredData);
   };
 
